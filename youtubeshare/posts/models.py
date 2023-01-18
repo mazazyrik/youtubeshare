51fc3a9f9bf1d37a64bbed394f32f43e59ca460e
mazazyrik <117200062+mazazyrik@users.noreply.github.com>
2023-01-18 18:08:32 +0300

    CSS load fail

---
+++ youtubeshare/posts/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Genre(models.Model):
+    genre = models.CharField(max_length=200)
+    slug = models.SlugField()
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.genre
+
+
+class Post(models.Model):
+    link = models.URLField()
+    text = models.TextField(max_length=400)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    genre = models.ForeignKey(
+        Genre,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='genre_key'
+    )
---
+++ youtubeshare/posts/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def index(request):
+    template = 'posts/index.html'
+    return render(request, template)
